30f8e36db318879ec05547564a18149469c7322b
kruglay <kruglay@mail.ru>
2017-03-14 21:39:58 +0300

    add new 'it' in webinfo_spec

--- spec/webinfo_spec.rb
+++ spec/webinfo_spec.rb
@@ -9,8 +9,14 @@ RSpec.describe Webinfo do
     expect(Webinfo.get_info('https://www.facebook.com/', node: 'img', attr: 'src')).to be_a_kind_of(Array)
   end
 
-  it "all elements of #get_info array should be string" do
+  it "all elements of #get_info array should be attributes of img node" do
+    nodes_name = []
+    response = HTTParty.get('https://www.facebook.com/')
+    doc = Nokogiri::HTML(response)
     arr_images = Webinfo.get_info('https://www.facebook.com/', node: 'img', attr: 'src')
-    expect(arr_images).to all be_a(String) if arr_images.count > 0
+    arr_attr = doc.xpath("//@src")
+    arr_images.each {|addr| nodes_name.push(doc.xpath("//*[@src='#{addr}']").first.name)}
+
+    expect(nodes_name).to all eq('img')
   end
 end
